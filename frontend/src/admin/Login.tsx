b504a22682b8ee5d1eb8ff23a2bda042364b47e3
ealinaa <alinaadhikari108@gmail.com>
2024-09-10 19:02:34 +0545

    worked with frontend

--- frontend/src/admin/Login.tsx
+++ frontend/src/admin/Login.tsx
@@ -1,15 +1,39 @@
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Mail } from "lucide-react"
 
 
 
 
 const Login = () => {
   return (
-    <div className="flex items-center justify-between h-screen w-screen">
-      <form className="border-2 border-solid border-gray-200 rounded-lg" >
-form of login
+
+    <div className="flex items-center justify-center min-h-screen">
+      <form className="md:p-8 w-full max-w-md border-2 md:border-solid border-gray-200 rounded-lg mx-4" >
+        <div className="mb-4">
+          <h1 className="font-bold text-2xl">Adhikari Eats</h1>
+        </div>
+        <div className="relative">
+
+
+          <Input
+            type="email"
+            placeholder="Enter Your email" className="pl-8"/>
+          <Mail className="absolute inset-y-2 left-2 text-gray-500 pointer-events-none"/>
+        </div>
+
+        <div>
+          {/* <Label>Password</Label> */}
+          <Input
+            type="password"
+            placeholder="Enter Your Password" />
+
+
+        </div>
+
       </form>
-      
-      </div>
+
+    </div>
   )
 }
 
