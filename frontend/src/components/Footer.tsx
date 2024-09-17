b74e2c717f1693f0cf6174368861c7f7270ec2cb
ealinaa <alinaadhikari108@gmail.com>
2024-09-17 19:22:58 +0545

    worked with frontend

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -2,7 +2,12 @@
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="bg-gray-800 text-center text-white pt-5 pb-4">
+      <p className="text-sm">
+        &copy; 2024 AdhikariEats. All rights reserved
+      </p>
+
+    </footer>
   )
 }
 
--- frontend/src/components/HeroSection.tsx
+++ frontend/src/components/HeroSection.tsx
@@ -1,34 +1,37 @@
 import { useState } from "react"
 import { Input } from "./ui/input"
-import { Search, SearchIcon } from "lucide-react"
+import { Search } from "lucide-react"
 import { Button } from "./ui/button"
+import HeroImage from "@/assets/pizzas.jpg"
 
 
 const HeroSection = () => {
-  const [searchText, setSearchText]= useState<string>("")
+  const [searchText, setSearchText] = useState<string>("")
   return (
     <div className="flex flex-col md:flex-row max-w-7xl mx-auto md:p-10 rounded-lg items-center justify-center m-4 gap-20 ">
-        <div className="flex flex-col gap-10 md:w-[40%] ">
-            <div className="flex flex-col gap-5">
-                <h1 className="font-bold md:font-extrabold md:text-5xl text-4xl">Order Food anytime and anywhere</h1>
-                <p className="text-gray-500">Hey! Our DElicious food is waiting for you, we are always near to you</p>
-
-            </div>
-            <div className="relative flex items-center gap-2">
-              <div className="">
-              <Input
-              type="text"
-              value={searchText}
-              onChange={(e) => setSearchText(e.target.value)}
-              className="pl-10  shadow-lg"
-            />
-            <Search className="text-gray-500 absolute inset-y-2 left-2"/>
-              </div>
-              <Button className="bg-orange-500 hover:bg-hoverOrange">Search</Button>
-             
-            </div>
+      <div className="flex flex-col gap-10 md:w-[40%] ">
+        <div className="flex flex-col gap-5">
+          <h1 className="font-bold md:font-extrabold md:text-5xl text-4xl">Order Food anytime and anywhere</h1>
+          <p className="text-gray-500">Hey! Our DElicious food is waiting for you, we are always near to you</p>
 
         </div>
+        <div className="relative flex items-center gap-2">
+
+          <Input
+            type="text"
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+            className="pl-10  shadow-lg"
+          />
+          <Search className="text-gray-500 absolute inset-y-2 left-2" />
+
+          <Button className="bg-orange-500 hover:bg-hoverOrange">Search</Button>
+
+        </div>
+      </div>
+      <div>
+        <img src={HeroImage}alt="" className="object-cover w-full max-h-[500px]" />
+      </div>
 
     </div>
   )
