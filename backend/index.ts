cf52a3ee0ac7572e79d32f169c519d7f52f6ab73
ealinaa <alinaadhikari108@gmail.com>
2024-09-22 22:02:27 +0545

    worked with controllers and mailtrap of backend

--- backend/index.ts
+++ backend/index.ts
@@ -1,12 +1,16 @@
 import express from "express"
 import dotenv from "dotenv";
 import connectDB from "./db/conectionDB";
+import UserRoute from "./routes/user.route"
 dotenv.config()
 
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+//api
+app.use("/api/v1/User", UserRoute)
 app.listen(PORT, () => {
     connectDB()
    
