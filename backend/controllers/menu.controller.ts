44c63222e54eb89cb9a80ca1c17e23f5b2c6ba5a
ealinaa <alinaadhikari108@gmail.com>
2024-09-26 19:07:15 +0545

    worked with controller of menu in backend

--- backend/controllers/menu.controller.ts
+++ backend/controllers/menu.controller.ts
@@ -1,53 +1,39 @@
-import { Request, Response } from "express";
-import uploadImageOnCloudinary from "../utils/imageUpload";
+import mongoose from "mongoose";
 import { Menu } from "../models/menu.model";
 import { Restaurant } from "../models/restaurant.model";
-import mongoose from "mongoose";
+import uploadImageOnCloudinary from "../utils/imageUpload";
+import { Request, Response } from "express";
 
-
-export const addMenu = async(req: Request, res: Response) => {
+export const addMenu = async (req:Request, res:Response) => {
     try {
-        
-        const {name, description, price} = req.body
-        const file= req.file
-        if(!file) {
-            return res.status(404).json({
-                success: false,
-                message: "Image is required"
+        const {name, description, price} = req.body;
+        const file = req.file;
+        if(!file){
+            return res.status(400).json({
+                success:false,
+                message:"Image is required"
             })
-        }
-        const imageUrl = await uploadImageOnCloudinary(file as Express.Multer.File)
-        const menu = await Menu.create({
-            name,
+        };
+        const imageUrl = await uploadImageOnCloudinary(file as Express.Multer.File);
+        const menu: any = await Menu.create({
+            name , 
             description,
             price,
-            image: imageUrl
-        })
-        const restaurant = await Restaurant.findOne({user:req.id})
-        if(restaurant) {
-            (restaurant.menus as mongoose.Schema.Types.ObjectId[]).push(menu._id)
-            await restaurant.save()
-            return res.status(201).json({
-                success: true,
-                message: "Menu added successfully",
-                menu
-            })
-
-
+            image:imageUrl
+        });
+        const restaurant = await Restaurant.findOne({user:req.id});
+        if(restaurant){
+            (restaurant.menus as mongoose.Schema.Types.ObjectId[]).push(menu._id); // array of menu's id
+            await restaurant.save();
         }
-    } catch (error) {
-       console.log(error) 
-       return res.status(500).json({message:"Internal server error"})
-    }
-}
 
-export const editMenu = async(req: Request, res: Response) => {
-    try {
-        const {id} = req.params
-        
+        return res.status(201).json({
+            success:true,
+            message:"Menu added successfully",
+            menu
+        });
     } catch (error) {
-        console.log(error)
-        return res.status(500).json({message:"Internal server error"})
-        
+        console.log(error);
+        return res.status(500).json({message:"Internal server error in addmenu"}); 
     }
 }
