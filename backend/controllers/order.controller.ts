2546b0041aaecb35e177464e8c1fd452b11872ad
ealinaa <alinaadhikari108@gmail.com>
2024-09-29 19:47:08 +0545

    worked with controller of Order in backend

--- backend/controllers/order.controller.ts
+++ backend/controllers/order.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { Order } from "../models/order.model";
+import { Restaurant } from "../models/restaurant.model";
 
 export const getOrders = async (req: Request, res: Response) => {
     try {
@@ -13,3 +14,24 @@ export const getOrders = async (req: Request, res: Response) => {
         return res.status(500).json({ success: false, message: "Internal server error" });
     }
 }
+
+export const createCheckoutSession = async (req: Request, res: Response) => {
+    try {
+      const checkoutSessionRequest: CheckoutSessionRequest = req.body;
+      const restaurant = await Restaurant.findById(
+        checkoutSessionRequest.restaurantId
+      ).populate("menu");
+      if (!restaurant) {
+        return res.status(400).json({
+          success: false,
+          message: "Restaurant not found",
+        });
+      }
+      const order = new Order({
+        restaurant: restaurant._id,
+        user: req.id, // id from token generated when user is authenticated is used
+        deliveryDetails: checkoutSessionRequest.deliveryDetails,
+        cartItems: checkoutSessionRequest.cartItems,
+        status: "pending",
+      });
+    }
